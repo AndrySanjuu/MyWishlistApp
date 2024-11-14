e12977f71c06230301c9174f5c3ead96f88be2e9
Andrei Mihai <a.mihai@reply.it>
2024-11-14 12:37:39 +0100

    initial commit

---
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -0,0 +1,28 @@
+package com.example.mywishlistapp
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun HomeView(){
+    val context = LocalContext.current
+
+    Scaffold(
+        topBar = {
+            AppBarView(title = "WhisList")
+            {
+                Toast.makeText(context, "button Clicked", Toast.LENGTH_LONG).show()
+            }
+        }
+    ) {
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(it) ){
+
+        }
+    }
+}
